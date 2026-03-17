2b8ef3cf46e2449922487f82bc55d0040e103e69
19JCA01-JoseContreras <j.contreras.10cdb@gmail.com>
2026-03-17 10:52:47 -0600

    Add files via upload
    Trabaje en clases

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+//Un programa que solicite al usuario un valor numerico
+//Mostrar en pantalla si positivo o negativo internal class Program
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Numeros Positivos y Negativos");
+    //Variables
+    int num;
+    num = Convert.ToInt32(Console.ReadLine());
+
+        if (num >= 0)
+            Console.WriteLine("Soy un numero POSITIVO");
+        else
+            Console.WriteLine("sOY UN NUMERO negativo");
+        
+    }
+}
