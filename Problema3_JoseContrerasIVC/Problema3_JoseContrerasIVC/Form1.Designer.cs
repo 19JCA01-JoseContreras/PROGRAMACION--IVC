9a63b44f2eab4d99366cc00413f0c7837193f3b2
19JCA01-JoseContreras <j.contreras.10cdb@gmail.com>
2026-09-21 19:37:35 -0600

    Add files via upload

---
+++ Problema3_JoseContrerasIVC/Problema3_JoseContrerasIVC/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Problema3_JoseContrerasIVC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            errorProvider1 = new ErrorProvider(components);
+            lblNombres = new Label();
+            lblEdad = new Label();
+            lblSalario = new Label();
+            lblDescripcion = new Label();
+            txtNombres = new TextBox();
+            txtEdad = new TextBox();
+            txtSalario = new TextBox();
+            txtDescripcion = new TextBox();
+            groupBox1 = new GroupBox();
+            button2 = new Button();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // lblNombres
+            // 
+            lblNombres.AutoSize = true;
+            lblNombres.Location = new Point(28, 34);
+            lblNombres.Name = "lblNombres";
+            lblNombres.Size = new Size(59, 15);
+            lblNombres.TabIndex = 0;
+            lblNombres.Text = "Nombres:";
+            // 
+            // lblEdad
+            // 
+            lblEdad.AutoSize = true;
+            lblEdad.Location = new Point(28, 77);
+            lblEdad.Name = "lblEdad";
+            lblEdad.Size = new Size(36, 15);
+            lblEdad.TabIndex = 1;
+            lblEdad.Text = "Edad:";
+            // 
+            // lblSalario
+            // 
+            lblSalario.AutoSize = true;
+            lblSalario.Location = new Point(28, 115);
+            lblSalario.Name = "lblSalario";
+            lblSalario.Size = new Size(45, 15);
+            lblSalario.TabIndex = 2;
+            lblSalario.Text = "Salario:";
+            // 
+            // lblDescripcion
+            // 
+            lblDescripcion.AutoSize = true;
+            lblDescripcion.Location = new Point(28, 160);
+            lblDescripcion.Name = "lblDescripcion";
+            lblDescripcion.Size = new Size(72, 15);
+            lblDescripcion.TabIndex = 3;
+            lblDescripcion.Text = "Descripcion:";
+            // 
+            // txtNombres
+            // 
+            txtNombres.Location = new Point(93, 31);
+            txtNombres.Name = "txtNombres";
+            txtNombres.Size = new Size(204, 23);
+            txtNombres.TabIndex = 4;
+            // 
+            // txtEdad
+            // 
+            txtEdad.Location = new Point(93, 69);
+            txtEdad.Name = "txtEdad";
+            txtEdad.Size = new Size(100, 23);
+            txtEdad.TabIndex = 5;
+            // 
+            // txtSalario
+            // 
+            txtSalario.Location = new Point(93, 107);
+            txtSalario.Name = "txtSalario";
+            txtSalario.Size = new Size(100, 23);
+            txtSalario.TabIndex = 6;
+            // 
+            // txtDescripcion
+            // 
+            txtDescripcion.Location = new Point(106, 143);
+            txtDescripcion.Multiline = true;
+            txtDescripcion.Name = "txtDescripcion";
+            txtDescripcion.ScrollBars = ScrollBars.Vertical;
+            txtDescripcion.Size = new Size(191, 56);
+            txtDescripcion.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(txtDescripcion);
+            groupBox1.Controls.Add(txtSalario);
+            groupBox1.Controls.Add(txtEdad);
+            groupBox1.Controls.Add(txtNombres);
+            groupBox1.Controls.Add(lblDescripcion);
+            groupBox1.Controls.Add(lblSalario);
+            groupBox1.Controls.Add(lblEdad);
+            groupBox1.Controls.Add(lblNombres);
+            groupBox1.Location = new Point(202, 61);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(405, 265);
+            groupBox1.TabIndex = 8;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ingrese Datos";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(289, 221);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 9;
+            button2.Text = "Enviar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(40, 221);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 8;
+            button1.Text = "Nuevo";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ErrorProvider errorProvider1;
+        private Label lblEdad;
+        private Label lblNombres;
+        private TextBox txtNombres;
+        private Label lblDescripcion;
+        private Label lblSalario;
+        private TextBox txtDescripcion;
+        private TextBox txtSalario;
+        private TextBox txtEdad;
+        private GroupBox groupBox1;
+        private Button button2;
+        private Button button1;
+    }
+}
