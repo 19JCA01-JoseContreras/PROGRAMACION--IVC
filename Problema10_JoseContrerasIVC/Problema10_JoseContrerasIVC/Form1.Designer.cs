a502441d621ce56a5177f2a6f2c0481f5df90893
19JCA01-JoseContreras <j.contreras.10cdb@gmail.com>
2026-09-22 16:57:09 -0600

    Add files via upload

---
+++ Problema10_JoseContrerasIVC/Problema10_JoseContrerasIVC/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Problema10_JoseContrerasIVC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dateTimePicker1 = new DateTimePicker();
+            listView1 = new ListView();
+            numericUpDown1 = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(304, 123);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(16, 23);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(418, 218);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(121, 97);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(455, 133);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(numericUpDown1);
+            Controls.Add(listView1);
+            Controls.Add(dateTimePicker1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private ListView listView1;
+        private NumericUpDown numericUpDown1;
+    }
+}
---
+++ Problema10_JoseContrerasIVC/Problema10_JoseContrerasIVC/Form1.cs
@@ -0,0 +1,10 @@
+namespace Problema10_JoseContrerasIVC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
